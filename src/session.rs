fa34d16bb6eb70fb184345df73e70b64d4819105
Guillaume Depardon <guillaume.depardon@gmail.com>
2019-08-05 04:59:54 +0200

    snake_case

--- src/session.rs
+++ src/session.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone)]
 pub struct Session {
-    pub fermenterOutput: f64,
+    pub fermenter_output: f64,
     pub bhefficient: f64,
     pub target: f64,
     pub recipe: String,
